149e81d1fb0daec2957e2ef15d775eecc056a5a7
StormEx <stormbox@tut.by>
2017-01-08 23:53:44 +0300

    initial commit

---
+++ make/HxDisposeMake.hx
@@ -0,0 +1,40 @@
+package ;
+
+import hxmake.haxelib.HaxelibExt;
+import hxmake.test.TestTask;
+import hxmake.idea.IdeaPlugin;
+import hxmake.haxelib.HaxelibPlugin;
+
+using hxmake.haxelib.HaxelibPlugin;
+
+class HxDisposeMake extends hxmake.Module {
+	function new() {
+		config.classPath = ["src"];
+		config.testPath = ["test"];
+		config.devDependencies = [
+			"utest" => "haxelib"
+		];
+
+		apply(HaxelibPlugin);
+		apply(IdeaPlugin);
+
+		library(
+			function(ext:HaxelibExt) {
+				ext.config.description = "Some stuff for help to dispose";
+				ext.config.contributors = ["storm_ex"];
+				ext.config.url = "https://github.com/StormEx/hxdispose";
+				ext.config.license = "MIT";
+				ext.config.version = "0.0.1";
+				ext.config.releasenote = "Initial release";
+				ext.config.tags = ["dispose", "disposable"];
+
+				ext.pack.includes = ["src", "haxelib.json", "README.md"];
+			}
+		);
+
+		var tt = new TestTask();
+		tt.targets = ["neko", "swf", "js", "node", "cpp", "java", "cs", "php"];
+		tt.libraries = ["hxdispose"];
+		task("test", tt);
+	}
+}

9ef71b4d989f64f6f2516c188adcb3a89e5a4048
StormEx <stormbox@tut.by>
2017-04-16 17:35:58 +0300

    added disposable array

---
+++ src/hxdispose/DisposableArray.hx
@@ -0,0 +1,17 @@
+package hxdispose;
+
+@:forward(push, length)
+abstract DisposableArray<T:IDisposable>(Array<T>) from Array<T> to Array<T> {
+	public function new(arr:Array<T>) {
+		this = arr;
+	}
+
+	public function dispose() {
+		if(this != null && this.length > 0)
+
+		for(e in this) {
+			Dispose.dispose(e);
+		}
+		this = null;
+	}
+}
